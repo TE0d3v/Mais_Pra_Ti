9fe93c6d08907bb567ec9a080a74a92d75c4865d
Pedro <pedrohenriqueteofilo2707@gmail.com>
2025-08-02 15:58:29 -0300

    aula 03 do 07

---
+++ Modulo-2-Front-End/aula10/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.getElementById('navToggle')
+    const navMenu = document.getElementById('navMenu')
+
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('open')
+    })
+})
+// o que está faltando
